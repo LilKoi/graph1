68cf743ee87f66c5195e620180c7017e99328a10
LilKoi <lil_koi@mail.ru>
2021-09-20 16:52:26 +0300

    create server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 const fs = require("fs");
 app.get('/', (req, res) => {
+    app.use('/data', express.static('data'))
+    app.use('/libs', express.static('libs'))
     res.sendFile(__dirname + '/index.html');
 })
 
