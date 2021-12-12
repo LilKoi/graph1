09d4dfc4be9f3ceedaedb8ee68630ea85b7e7543
LilKoi <Lil_koi@mail.ru>
2021-12-12 13:07:39 +0300

    алгоритм дейкстра готов

--- public/display.js
+++ public/display.js
@@ -351,3 +351,26 @@ let dostizhimost = document.getElementById("dostizhimost").addEventListener("cli
   } 
   console.log(cmej)
 })
+
+let dijkstra = document.getElementById("dijkstra").addEventListener("click", (e) => {
+  e.preventDefault()
+  let cmej = state.pullSetka()
+  Dijkstra(cmej)
+  function Dijkstra(matrix, start = 0) {
+    const rows = matrix.length,
+    cols = matrix[0].length;
+    const distance = new Array(rows).fill(Infinity);
+    distance[start] = 0;
+    for(let i = 0; i < rows; i++) {
+      if(distance[i] < Infinity) {
+       for(let j = 0; j < cols; j++) {
+        if(matrix[i][j] + distance[i] < distance[j]) {
+            distance[j] = matrix[i][j] + distance[i];
+        }
+       }
+       console.log(distance);
+      }
+  }
+  return distance;
+}
+})
